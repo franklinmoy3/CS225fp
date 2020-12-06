6a5b1d5dde6f51761224381cdc6ef7544ab69dd7
Franklin Moy <fmoy3@illinois.edu>
2020-12-05 22:20:07 -0600

    adapted readFromFile to parse the CSV-formatted dataset

--- readFromFile.cpp
+++ readFromFile.cpp
@@ -19,17 +19,23 @@ std::string file_to_string(const std::string & filename) {
 	return strStream.str();
 }
 
-std::vector<std::string> file_to_vector(const std::string & filename) {
-	std::ifstream text(filename);
-	std::vector<std::string> out;
+std::vector<std::vector<std::string>> file_to_vector(const std::string & filename) {
+	// Create file stream and return vector
+	std::ifstream fileStream(filename);
+	std::string currLine;
+	std::vector<std::vector<std::string>> out;
 
-	if (text.is_open()) {
-		std::istream_iterator<std::string> iter(text);
-		while (!text.eof()) {
-			out.push_back(*iter);
-			++iter;
+	// Read line by line from file (CSV) stream and place each token into output vector
+	if (fileStream.is_open()) {
+		while (std::getline(fileStream, currLine)) {
+			std::istringstream currLineStream(currLine);
+			std::string elem;
+			std::vector<std::string> lineVec;
+			while (std::getline(currLineStream, elem, ',')) {
+				lineVec.push_back(elem);
+			}
+			out.push_back(lineVec);
 		}
 	}
-
 	return out;
 } 
--- readFromFile.h
+++ readFromFile.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <string>
 
-// adapted from CS 225 Final Project AMA
-
 /** 
  * Reads a file into a string
  * @param filename The file to read from 
@@ -13,6 +11,6 @@ std::string file_to_string(const std::string & filename);
 /** 
  * Reads a file into a vector
  * @param filename The file to read from
- * @return a vector containing filename's contents
+ * @return a vector of vectors containing elements from each line from CSV
  */
-std::vector<std::string> file_to_vector(const std::string & filename);
+std::vector<std::vector<std::string>> file_to_vector(const std::string & filename);
