57c9bd63a6eaced378f9f2c9f6bef6661717ecfb
Franklin Moy <fmoy3@illinois.edu>
2020-12-07 23:38:10 -0600

    created test case for parse_CSV

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,23 +1,20 @@
 #include <string>
 #include <vector>
 
-#include "/catch/catch.hpp"
+#include "catch/catch.hpp"
 #include "../readFromFile.h"
 
 
-TEST_CASE("Verify that file_to_string works on a small example") {
-	std::string res = file_to_string("tests/smallSample.txt");
-	// always check "expected" == "actual" --> be consistent
-	REQUIRE("hello\nstudents\nof\ncs\n225\n!!!\n" == res);
-}
-
-TEST_CASE("Verify that file_to_vector works on a small example") {
-	std::vector<std::string> res = file_to_vector("tests/smallSample.txt");
-	std::string expected[] = {"hello", "students", "of", "cs", "225", "!!!"};
+TEST_CASE("parse_CSV correctly parses a small sample") {
+	std::vector<std::vector<std::string>> res = parse_CSV("tests/testParse.csv");
+	std::vector<std::vector<std::string>> expected = { {"akshayb3", "1", },
+													   {"fmoy3", "2"}, 
+													   {"vg12", "3"} };
 	
-	REQUIRE(6 == res.size());
+	REQUIRE(3 == res.size());
 
 	for (int i = 0; i < res.size(); i++) {
 		REQUIRE(expected[i] == res[i]);
 	}
 }
+
